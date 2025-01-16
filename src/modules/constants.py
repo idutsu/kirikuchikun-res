c12f897161c5978270e56c516c76f9d32ccc5c65
idutsu <email@idutsu.com>
2025-01-17 02:22:44 +0900

    edit

--- src/modules/constants.py
+++ src/modules/constants.py
@@ -2,6 +2,7 @@ USER_DIR = "/home/ubuntu"
 NLP_DIR = f"{USER_DIR}/nlp"
 WIKIPEDIA_TEXT = f"{NLP_DIR}/corpus/wikipedia.txt"
 WIKIPEDIA_TEST_TEXT = f"{NLP_DIR}/corpus/test.txt"
+WIKIPEDIA_SENT_TEXT = f"{NLP_DIR}/corpus/wikipedia_sent.txt"
 
 
 
